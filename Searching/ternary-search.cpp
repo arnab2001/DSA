a68b04969cc9e4ad03d7df68eec3ce4af7de3f12
Het Patel <56436879+whohet@users.noreply.github.com>
2021-10-02 20:39:42 +0530

    Added ternary search implementation

---
+++ Searching/ternary-search.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//A program to perform ternary search
+//Ternary Search is a searching technique, used to determine a position of a specific vale in an (sorted) array in order of O(log3(n)) time
+
+void search(string s, string t)
+{
+    string concat = t + "$" + s;
+    int n = concat.length();
+
+    int z[]
+}
+
+int main()
+{
+    string s = "Hello,World";
+    string t = "llo";
+    search(s, t);
+    return 0;
+}
