2753d3c1f701fee848bc5c955c3548aef3388ddf
Sreemoyee Sadhukhan <77785024+Sreemoyee26@users.noreply.github.com>
2021-10-08 14:46:40 +0530

    Added Selection Sort in C++

---
+++ Sorting/SelectionSort.cpp
@@ -0,0 +1,50 @@
+// C++ program for selection sort
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+ 
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_idx;
+ 
+    // One by one move boundary of unsorted subarray
+    for (i = 0; i < n-1; i++)
+    {
+        // Find the minimum element in unsorted array
+        min_idx = i;
+        for (j = i+1; j < n; j++)
+        if (arr[j] < arr[min_idx])
+            min_idx = j;
+ 
+        // Swap the found minimum element with the first element
+        swap(&arr[min_idx], &arr[i]);
+    }
+}
+ 
+// Function to print the array
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+ 
+// Driver code
+int main()
+{
+    int arr[] = {4, 25, 14, 23, 11, -7, 44, -13, 25};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    selectionSort(arr, n);
+    cout << "Sorted array: \n";
+    printArray(arr, n);
+    return 0;
+}
+
+// This code is contributed by Sreemoyee Sadhukhan
