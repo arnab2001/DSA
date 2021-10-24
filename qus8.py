4c0ba9116fe76e26f12dad61595a994cbdf373db
senpai0666 <50199317+senpai0666@users.noreply.github.com>
2021-10-24 18:52:31 +0530

    Add files via upload

---
+++ qus8.py
@@ -0,0 +1,13 @@
+n = 6
+k = (n * 2) - 1
+for i in range(1, 7):
+    for j in range(0, k):
+        print("", end=" ")
+    c = (i * 2) - 1
+    for j in range(1, c - 1):
+        if j == 1 or j == c - 2:
+            print(i - 1, "", end=" ")
+        else:
+            print("*", end=" ")
+    print("\n")
+    k = k - 2
