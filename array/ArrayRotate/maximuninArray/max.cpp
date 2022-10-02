ac3647248e6827f036bad79068c02a348784a53a
Ankush Jagga <ankushjagga22@gmail.com>
2022-10-02 16:33:02 +0530

    maxInArray in cpp

--- array/ArrayRotate/maximuninArray/max.cpp
+++ array/ArrayRotate/maximuninArray/max.cpp
@@ -15,34 +15,25 @@ void printArray(int*arr,int n){
     cout<<endl;
     
 }
-void reverseArray(int *arr,int n){
-    int start =0;
-    int end=n-1;
- while (start<=end)
- {
-    int temp=arr[start];
-    arr[start]=arr[end];
-    arr[end]=temp;
-    start++;
-    end--;
- }
- 
+
+int maxinArray(int *arr,int n){
+    int max=INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    return max;
     
 }
-
 int main(){
-    int n;
+ int n;
     cout<<"enter size of array :"<<endl;
     cin>>n;
 int arr[100];
 cout<<"enter elements in array:";
 inputArray(arr,n);
-cout<<"array before reverse:";
-printArray(arr,n);
-cout<<endl;
-cout<<"Array after reverse:";
-reverseArray(arr,n);
-printArray(arr,n);
-
+cout<<"max element in array is "<<maxinArray(arr,n);
 return 0;
 }
